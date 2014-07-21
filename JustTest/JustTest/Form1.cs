4af7cd3e980ce417d94a4d45f8e4f9114caa77b9
jihuaguo <jhg369@qq.com>
2014-07-21 18:50:51 +0800

    JustTest
    Test

---
+++ JustTest/JustTest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace JustTest
+{
+    public partial class Form1 : Form
+    {
+        private const string constString = "jihauguo";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(Form1.constString);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
